e4ca1ed04747fc254779f20ba64965d2366dfa3b
Nir Cohen <nircoe@gmail.com>
2025-12-17 18:47:56 +0200

    [Testing]: Implement Basic OpenGL scene with gamecoe (#21)
    - Remove all OpenGL (glm/glfw/glad) related code from main.cpp - only gamecoe usage
    - Create a basic scene with a cube
    - Full gamecoe hierarchy (Game->Scene->GameObject->Component)

--- include/gamecoe.hpp
+++ include/gamecoe.hpp
@@ -1,12 +1,20 @@
 #pragma once
 
 // gamecoe headers
+#include <gamecoe/core/game.hpp>
 #include <gamecoe/core/window.hpp>
-#include <gamecoe/entity/camera.hpp>
+#include <gamecoe/core/scene.hpp>
+
 #include <gamecoe/graphics/shader.hpp>
 #include <gamecoe/graphics/texture.hpp>
 
+#include <gamecoe/entity/game_object.hpp>
+#include <gamecoe/entity/renderer/renderer.hpp>
+#include <gamecoe/entity/renderer/shape_renderer.hpp>
+#include <gamecoe/entity/camera.hpp>
+
 // gamecoe toolkit libraries headers
+#include <colorcoe.hpp>
 #include <timecoe.hpp>
 #include <logcoe.hpp>
 
--- main.cpp
+++ main.cpp
@@ -1,255 +1,27 @@
-#include <glad/gl.h>
-#include <GLFW/glfw3.h>
 #include <gamecoe.hpp>
 #include <iostream>
 #include <cmath>
 #include <optional>
-#include <stb/stb_image.h>
-#include <glm/glm.hpp>
-#include <glm/gtc/matrix_transform.hpp>
-#include <glm/gtc/type_ptr.hpp>
-
-const int WINDOW_WIDTH = 800;
-const int WINDOW_HEIGHT = 600;
-float lastX = 400.0f;
-float lastY = 300.0f;
-float yaw = -90.0f;
-float pitch = 0.0f;
-float fov = 45.0f;
-bool firstMouse = true;
-glm::vec3 cameraPos(0.0f, 0.0f, 3.0f);
-glm::vec3 cameraFront(0.0f, 0.0f, -1.0f);
-glm::vec3 cameraUp(0.0f, 1.0f, 0.0f);
-
-void framebufferSizeCallback([[maybe_unused]] GLFWwindow *window, int width, int height)
-{
-    glViewport(0, 0, width, height);
-}
-
-void processInput(GLFWwindow *window)
-{
-    const float cameraSpeed = 10.0f * timecoe::deltaTime();
-    if (glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS)
-        cameraPos += cameraSpeed * cameraFront;
-    if (glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS)
-        cameraPos -= cameraSpeed * cameraFront;
-    if (glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS)
-        cameraPos -= glm::normalize(glm::cross(cameraFront, cameraUp)) * cameraSpeed;
-    if (glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS)
-        cameraPos += glm::normalize(glm::cross(cameraFront, cameraUp)) * cameraSpeed;
-
-    if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
-        glfwSetWindowShouldClose(window, true);
-}
-
-void mouseCallback([[maybe_unused]] GLFWwindow *window, double xpos, double ypos)
-{
-    if (firstMouse)
-    {
-        lastX = xpos;
-        lastY = ypos;
-        firstMouse = false;
-    }
-    float xOffset = xpos - lastX;
-    float yOffset = ypos - lastY;
-    lastX = xpos;
-    lastY = ypos;
-    const float sensitivity = 0.1f;
-    xOffset *= sensitivity;
-    yOffset *= sensitivity;
-
-    yaw += xOffset;
-    pitch -= yOffset;
-
-    pitch = glm::clamp(pitch, -89.0f, 89.0f);
-
-    glm::vec3 direction;
-    direction.x = glm::cos(glm::radians(yaw)) * glm::cos(glm::radians(pitch));
-    direction.y = glm::sin(glm::radians(pitch));
-    direction.z = glm::sin(glm::radians(yaw)) * glm::cos(glm::radians(pitch));
-
-    cameraFront = glm::normalize(direction);
-}
-
-void scrollCallback([[maybe_unused]] GLFWwindow *window, [[maybe_unused]] double xoffset, double yoffset)
-{
-    fov -= yoffset;
-    fov = glm::clamp(fov, 1.0f, 45.0f);
-}
-
-struct GarbageCollector
-{
-    unsigned int m_VAO = 0;
-    unsigned int m_VBO = 0;
-    unsigned int m_EBO = 0;
-    unsigned int m_texture1 = 0;
-    unsigned int m_texture2 = 0;
-    ~GarbageCollector()
-    {
-        glDeleteTextures(1, &m_texture1);
-        glDeleteTextures(1, &m_texture2);
-        glDeleteVertexArrays(1, &m_VAO);
-        glDeleteBuffers(1, &m_VBO);
-        glDeleteBuffers(1, &m_EBO);
-    }
-};
 
 int main()
 {
-    gamecoe::Window window("gamecoe", WINDOW_WIDTH, WINDOW_HEIGHT);
+    gamecoe::Game game;
 
+    auto &camera = game.mainCamera();
+    camera.owner().transform().translate({3.0f, 3.0f, 5.0f});
+    camera.owner().transform().lookAt({0.0f, 0.0f, 0.0f});
 
-    // glfwSetFramebufferSizeCallback(window, framebufferSizeCallback);
-    // glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
-    // glfwSetCursorPosCallback(window, mouseCallback);
-    // glfwSetScrollCallback(window, scrollCallback);
+    auto &scene = game.createScene("tester");
+    auto &shape = scene.createGameObject("shape");
+    shape.setRenderer(gamecoe::ShapeRenderer::cube(shape, colorcoe::maroon()));
+    // shape.transform().setScale({0.5f, 0.5f, 0.5f});
+    // shape.transform().rotate({0.0f, 0.0f, 45.0f});
+    // shape.transform().translate({0.25f, 0.3f, 0.0f});
 
-    // build and compile our shader program
-    // ------------------------------------
+    game.loadScene("tester");
+    game.activateScene("tester");
 
-    gamecoe::Shader shader("assets/shaders/shader.vert", "assets/shaders/shader.frag");
-
-    GarbageCollector gc;
-
-    // vertex data and buffers
-    float vertices[] = {
-        -0.5f, -0.5f, -0.5f, 0.0f, 0.0f,
-        0.5f, -0.5f, -0.5f, 1.0f, 0.0f,
-        0.5f, 0.5f, -0.5f, 1.0f, 1.0f,
-        0.5f, 0.5f, -0.5f, 1.0f, 1.0f,
-        -0.5f, 0.5f, -0.5f, 0.0f, 1.0f,
-        -0.5f, -0.5f, -0.5f, 0.0f, 0.0f,
-
-        -0.5f, -0.5f, 0.5f, 0.0f, 0.0f,
-        0.5f, -0.5f, 0.5f, 1.0f, 0.0f,
-        0.5f, 0.5f, 0.5f, 1.0f, 1.0f,
-        0.5f, 0.5f, 0.5f, 1.0f, 1.0f,
-        -0.5f, 0.5f, 0.5f, 0.0f, 1.0f,
-        -0.5f, -0.5f, 0.5f, 0.0f, 0.0f,
-
-        -0.5f, 0.5f, 0.5f, 1.0f, 0.0f,
-        -0.5f, 0.5f, -0.5f, 1.0f, 1.0f,
-        -0.5f, -0.5f, -0.5f, 0.0f, 1.0f,
-        -0.5f, -0.5f, -0.5f, 0.0f, 1.0f,
-        -0.5f, -0.5f, 0.5f, 0.0f, 0.0f,
-        -0.5f, 0.5f, 0.5f, 1.0f, 0.0f,
-
-        0.5f, 0.5f, 0.5f, 1.0f, 0.0f,
-        0.5f, 0.5f, -0.5f, 1.0f, 1.0f,
-        0.5f, -0.5f, -0.5f, 0.0f, 1.0f,
-        0.5f, -0.5f, -0.5f, 0.0f, 1.0f,
-        0.5f, -0.5f, 0.5f, 0.0f, 0.0f,
-        0.5f, 0.5f, 0.5f, 1.0f, 0.0f,
-
-        -0.5f, -0.5f, -0.5f, 0.0f, 1.0f,
-        0.5f, -0.5f, -0.5f, 1.0f, 1.0f,
-        0.5f, -0.5f, 0.5f, 1.0f, 0.0f,
-        0.5f, -0.5f, 0.5f, 1.0f, 0.0f,
-        -0.5f, -0.5f, 0.5f, 0.0f, 0.0f,
-        -0.5f, -0.5f, -0.5f, 0.0f, 1.0f,
-
-        -0.5f, 0.5f, -0.5f, 0.0f, 1.0f,
-        0.5f, 0.5f, -0.5f, 1.0f, 1.0f,
-        0.5f, 0.5f, 0.5f, 1.0f, 0.0f,
-        0.5f, 0.5f, 0.5f, 1.0f, 0.0f,
-        -0.5f, 0.5f, 0.5f, 0.0f, 0.0f,
-        -0.5f, 0.5f, -0.5f, 0.0f, 1.0f};
-    // unsigned int indices[] = {
-    //     // note that we start from 0!
-    //     0, 1, 3, // first triangle
-    //     1, 2, 3  // second triangle
-    // };
-
-    unsigned int VAO, VBO, EBO = 0;
-    glGenVertexArrays(1, &VAO);
-    gc.m_VAO = VAO;
-    glGenBuffers(1, &VBO);
-    gc.m_VBO = VBO;
-    // glGenBuffers(1, &EBO);
-    gc.m_EBO = EBO;
-
-    glBindVertexArray(VAO);
-
-    glBindBuffer(GL_ARRAY_BUFFER, VBO);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
-
-    // glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
-    // glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
-
-    // position attribute
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void *)0);
-    glEnableVertexAttribArray(0);
-    // texture attribute
-    glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void *)(3 * sizeof(float)));
-    glEnableVertexAttribArray(1);
-
-    // first texture
-    gamecoe::Texture texture1("assets/images/container.jpg");
-    texture1.setParameters(gamecoe::TextureWrap::Repeat, 
-                           gamecoe::TextureWrap::Repeat, 
-                           gamecoe::TextureFilter::LinearMipmapLinear, 
-                           gamecoe::TextureFilter::Linear);
-
-    // second texture
-    gamecoe::Texture texture2("assets/images/awesomeface.png");
-    texture2.setParameters(gamecoe::TextureWrap::Repeat, 
-                           gamecoe::TextureWrap::Repeat, 
-                           gamecoe::TextureFilter::LinearMipmapLinear, 
-                           gamecoe::TextureFilter::Linear);
-
-    shader.use();
-    shader.set("texture1", 0);
-    shader.set("texture2", 1);
-
-    glm::vec3 cubePositions[]{
-        glm::vec3(0.0f, 0.0f, 0.0f),
-        glm::vec3(2.0f, 5.0f, -15.0f),
-        glm::vec3(-1.5f, -2.2f, -2.5f),
-        glm::vec3(-3.8f, -2.0f, -12.3f),
-        glm::vec3(2.4f, -0.4f, -3.5f),
-        glm::vec3(-1.7f, 3.0f, -7.5f),
-        glm::vec3(1.3f, -2.0f, -2.5f),
-        glm::vec3(1.5f, 2.0f, -2.5f),
-        glm::vec3(1.5f, 0.2f, -1.5f),
-        glm::vec3(-1.3f, 1.0f, -1.5f)};
-
-    glEnable(GL_DEPTH_TEST);
-
-    // wire mode
-    // glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
-
-    // main loop
-    while (window.active())
-    {
-        // processInput(window);
-
-        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
-        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-
-        glActiveTexture(GL_TEXTURE0);
-        texture1.bind();
-        glActiveTexture(GL_TEXTURE1);
-        texture2.bind();
-
-        // Camera
-        glm::mat4 view(glm::lookAt(cameraPos, cameraPos + cameraFront, cameraUp));
-        glm::mat4 projection(glm::perspective(glm::radians(fov), (float)WINDOW_WIDTH / WINDOW_HEIGHT, 0.1f, 100.0f));
-
-        shader.use();
-        shader.set("view", view);
-        shader.set("projection", projection);
-
-        glBindVertexArray(VAO);
-        for (int i = 0; i < 10; ++i)
-        {
-            // cubePositions
-            glm::mat4 model(1.0f);
-            model = glm::translate(model, cubePositions[i]);
-            model = glm::rotate(model, (10 - i) * (float)glfwGetTime() * glm::radians((i + 1) * 20.0f), glm::vec3(1.0f + i, 0.3f - i, i * 0.5f));
-            shader.set("model", model);
-            glDrawArrays(GL_TRIANGLES, 0, 36);
-        }
-    }
+    game.play();
 
     return 0;
 }
